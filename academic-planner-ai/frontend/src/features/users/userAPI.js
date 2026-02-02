5f88556ce664cc8b6dc35856d76c12bc84684900
Preetha Jagan <preethavjjagan@gmail.com>
2026-02-02 17:43:29 +0530

    Update userAPI.js

--- academic-planner-ai/frontend/src/features/users/userAPI.js
+++ academic-planner-ai/frontend/src/features/users/userAPI.js
@@ -2,12 +2,13 @@ import api from '../../services/api';
 
 export const userAPI = {
   getProfile: async () => {
-    const response = await api.get('/api/users/profile');
+    const response = await api.get('/users/profile');
     return response.data;
   },
   updateProfile: async (userData) => {
-    const response = await api.put('/api/users/profile', userData);
+    const response = await api.put('/users/profile', userData);
     return response.data;
   },
 };
+
 
